c7e42d5fd59d649d69773b533da91a80f6d6e9b4
ramotk02 <omartaky.ot@gmail.com>
2024-01-17 12:50:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+
+function App() {
+  return (
+    <section>
+    <div className='Card'>
+      <div className='icon'>
+      </div>
+      <p>Omar</p> 
+      <p>20yo</p>
+      <p>student</p> 
+    </div>
+
+    <div className='Card'>
+      <div className='icon'>
+      </div>
+      <p>Rashid</p> 
+      <p>20yo</p>
+      <p>student</p> 
+    </div>
+
+    <div className='Card'>
+      <div className='icon'>
+      </div>
+      <p>Hafsa</p> 
+      <p>20yo</p>
+      <p>student</p> 
+    </div>
+
+    <div className='Card'>
+      <div className='icon'>
+      </div>
+      <p>Meriem</p> 
+      <p>20yo</p>
+      <p>student</p> 
+    </div>
+
+
+
+
+    </section>
+  
+  );
+}
+
+export default App;
