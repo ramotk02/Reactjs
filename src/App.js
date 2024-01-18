40f45891dc0e87cbe7fc96d3775d129b550495d8
ramotk02 <omartaky.ot@gmail.com>
2024-01-18 16:19:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,54 @@
-import './App.css';
+import "./App.css";
+
+const students = [
+  {
+    name: "Omar",
+    age: 20,
+    status: "Student",
+    text: "ggjierfv ghuerighurehogfr greufio g eergurwi greuwigoeo greuger egergiopfsg ueor vjfgve",
+  },
+  {
+    name: "Rashid",
+    age: 21,
+    status: "Student",
+    text: "ggjierfv ghuerighurehogfr greufio g eergurwi greuwigoeo greuger egergiopfsg ueor vjfgve",
+  },
+  {
+    name: "Hafsa",
+    age: 24,
+    status: "Student",
+    text: "ggjierfv ghuerighurehogfr greufio g eergurwi greuwigoeo greuger egergiopfsg ueor vjfgve",
+  },
+  {
+    name: "Meriem",
+    age: 36,
+    status: "Student",
+    text: "ggjierfv ghuerighurehogfr greufio g eergurwi greuwigoeo greuger egergiopfsg ueor vjfgve",
+  },
+];
+
+function Card(props) {
+  return (
+    <section>
+      <div className="Card">
+        <div className="icon"></div>
+        <p>{props.name || "Inconito"}</p>
+        <p> {props.age || "None"} </p>
+        <p> {props.status || "None"}</p>
+        <p>{props.text || "None"}</p>
+      </div>
+      <style></style>
+    </section>
+  );
+}
 
 function App() {
   return (
-    <section>
-    <div className='Card'>
-      <div className='icon'>
-      </div>
-      <p>Omar</p> 
-      <p>20yo</p>
-      <p>student</p> 
+    <div className="big-container">
+      {students.map((item) => (
+        <Card name={item.name}  age={item.age} status={item.status} text={item.text} />
+      ))}
     </div>
-
-    <div className='Card'>
-      <div className='icon'>
-      </div>
-      <p>Rashid</p> 
-      <p>20yo</p>
-      <p>student</p> 
-    </div>
-
-    <div className='Card'>
-      <div className='icon'>
-      </div>
-      <p>Hafsa</p> 
-      <p>20yo</p>
-      <p>student</p> 
-    </div>
-
-    <div className='Card'>
-      <div className='icon'>
-      </div>
-      <p>Meriem</p> 
-      <p>20yo</p>
-      <p>student</p> 
-    </div>
-
-
-
-
-    </section>
-  
   );
 }
 
