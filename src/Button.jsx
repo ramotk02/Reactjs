b26b6fb0b68c45945e5a582f23f015d9e8f150e5
ramotk02 <omartaky.ot@gmail.com>
2024-01-24 09:35:56 +0100

    up

--- src/Button.jsx
+++ src/Button.jsx
@@ -4,7 +4,7 @@ const Button = () => {
   const handleClick = () => {
     console.log("click");
   };
-  return <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">Cick on me</button>;
+  return <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded opacity-75">Cick on me</button>;
 };
 
 export default Button;
