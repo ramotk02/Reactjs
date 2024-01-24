bc5074954b7e1b0bf0426f62399f2792ff417441
ramotk02 <omartaky.ot@gmail.com>
2024-01-24 09:18:00 +0100

    up logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,9 +5,7 @@ import logo from "./assests/pic/GMC.png"
 const Header = () => {
   return (
     <header className='flex justify-between px-[20px] py-[20px] bg-[#0101] '>
-      <div className='h-[30px] w-[20%] flex justify-center'>
-        <img src={logo} alt="gmc-pic" />
-      </div>
+        <img src={logo} alt="gmc-pic" className=' h-[25px]' />
 
         <nav>
             <ul className='flex gap-[50px]'>
